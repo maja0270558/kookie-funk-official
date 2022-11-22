b3b280e39b00aaa2f1503d56274947ba007726dd
Django <maja0270558@gmail.com>
2022-11-22 16:20:10 +0800

    Add firebase admin sdk

--- pages/middleware.ts
+++ pages/middleware.ts
@@ -1,9 +1,10 @@
 // middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+    const { cookies } = request;
+    const jwt = cookies.OutsiteJWT;
     return NextResponse.redirect(new URL("/about-2", request.url));
 }
 
