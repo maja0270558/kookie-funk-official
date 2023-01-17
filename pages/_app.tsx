165956c3c6d339d85670433a023e2f29f17c4cb2
Django <maja0270558@gmail.com>
2023-01-17 10:24:49 +0800

    Implement session provider for next-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,23 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 import { Layout } from "../components/Layout";
 import { store } from "..//store";
 import { Provider } from "react-redux";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <Provider store={store}>
-            <Layout>
-                <Component {...pageProps} />{" "}
-            </Layout>
-        </Provider>
+        <SessionProvider
+            // Provider options are not required but can be useful in situations where
+            // you have a short session maxAge time. Shown here with default values.
+            session={pageProps.session}
+        >
+            <Provider store={store}>
+                <Layout>
+                    <Component {...pageProps} />{" "}
+                </Layout>
+            </Provider>
+        </SessionProvider>
     );
 }
 
