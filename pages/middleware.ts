acb1df1b26c5b537f6167ae5abfddf4c15c71d29
Django <maja0270558@gmail.com>
2023-01-17 09:45:17 +0800

    Remove firebase

--- pages/middleware.ts
+++ pages/middleware.ts
@@ -4,7 +4,6 @@ import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
     const { cookies } = request;
-    const jwt = cookies.OutsiteJWT;
     return NextResponse.redirect(new URL("/about-2", request.url));
 }
 
