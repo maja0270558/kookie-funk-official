9d984d5ff35d25e36f1f4964be2be4a426c902dc
django <maja0270558@gmail.com>
2023-01-24 14:08:35 +0800

    Implement detail page

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -9,12 +9,36 @@ const works = () => {
 
 
     const content = `
-# h1 Heading 8-)
-## h2 Heading
-### h3 Heading
-#### h4 Heading
-##### h5 Heading
-###### h6 Heading`
+1. Lorem ipsum dolor sit amet
+2. Consectetur adipiscing elit
+3. Integer molestie lorem at massa
+1. You can use sequential numbers...
+1. ...or keep all the numbers as 
+
+| Option | Description |
+| ------ | ----------- |
+| data   | path to data files to supply the data that will be passed into templates. |
+| engine | engine to be used for processing templates. Handlebars is the default. |
+| ext    | extension to be used for dest files. |
+## Links
+
+[link text](http://dev.nodeca.com)
+
+[link with title](http://nodeca.github.io/pica/demo/ "title text!")
+
+Autoconverted link https://github.com/nodeca/pica (enable linkify to see)
+
+**This is bold text**
+
+__This is bold text__
+
+*This is italic text*
+
+_This is italic text_
+
+`
+
+    const detail = `path to data files to supply the data that will be passed into templates.`
     // const { data, error, isLoading } = useSWR('/api/get/works', (url) => fetch(url).then(res => res.json()));
 
     // if (error) return <div>failed to load</div>
@@ -31,43 +55,58 @@ const works = () => {
 
     // const compoment = data.data.map((workData: WorksData) => <Gallery works={workData} />);
 
-    return (
-        <div className="flex-row min-h-full min-w-full bg-yello-50">
-            <div className="flex flex-col lg:flex-row min-h-full bg-green-100">
-                {/* Image PART */}
 
-                <div className="flex grow-[1] lg:grow-[5] max-h-screen bg-blue-200">
-                    <div className="flex items-center flex-1 justify-center bg-pink-300">
+    return (
+        <div className="flex flex-col" >
+            <div className="flex flex-row  place-content-center min-h-screen p-2">
+                <div className="flex flex-col lg:flex-row flex-1 ">
+                    <div className="relative flex flex-auto justify-center min-w-[368]">
                         <Image
                             alt=""
-                            src={"/2.jpeg"}
+                            // src={"/profile.jpg"}
+                            src={"/1.jpeg"}
                             width="0"
                             height="0"
-                            sizes="(max-width: 1024px) 90vw, 60vw"
-                            className=" object-fill w-auto h-auto aspect-auto max-w-[768px] sm: min-w-[368px]"
+                            sizes="100vw"
+                            className="object-contain lg:object-contain w-auto h-auto aspect-auto p-2"
                         />
-
-
                     </div>
 
-                </div>
-                {/* MARK PART */}
-
-                <div className="flex grow-[5] lg:grow-[1] bg-slate-500">
-                    <div className="flex flex-col lg:flex-col-reverse w-full bg-slate-600">
-                        <div className=" editor">
-                            <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                                {content}
+                    <div className="flex lg:flex-1 p-8">
+                        <div className="flex lg:items-end editor">
+                            <ReactMarkdown className="flex flex-col" remarkPlugins={[remarkGfm]}>
+                                {detail}
                             </ReactMarkdown>
                         </div>
                     </div>
+
                 </div>
             </div>
-
-            <div className="bg-green-300 h-36"></div>
+            <div className="flex flex-1 p-8">
+                <div className="flex lg:items-end editor">
+                    <ReactMarkdown className="flex flex-col" remarkPlugins={[remarkGfm]}>
+                        {content}
+                    </ReactMarkdown>
+                </div>
+            </div>
+            <div className="flex flex-1 p-2">
+                <div className="carousel carousel-center w-full p-4 space-x-4  ">
+                    <div className="carousel-item">
+                        <img src="/1.jpeg" className=" object-cover w-36 h-36" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="/2.jpeg" className=" object-cover w-36 h-36" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="/profile.jpg" className=" object-cover w-36 h-36" />
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 
 };
+
 
 export default works;
