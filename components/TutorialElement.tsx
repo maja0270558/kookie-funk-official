cb6ce055df7e9e4a959d4b13124a3e779a0a7853
Django <maja0270558@gmail.com>
2023-01-31 15:30:10 +0800

    Implement course page

---
+++ components/TutorialElement.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import classNames from "classnames";
+
+type TutorialProps = {
+    src: string;
+    link: string;
+    title: string;
+    desc: string;
+};
+
+function TutorialElement(props: TutorialProps) {
+    return (
+        <Link href={props.link} target="_blank">
+            <div className="flex flex-col lg:flex-row p-8">
+                <img
+                    className=" w-full object-cover lg:w-[60vh] lg:h-auto"
+                    src={props.src}
+                    alt=""
+                />
+                <div className="p-6 flex flex-col justify-center">
+                    <h5 className="text-base-content text-xl font-bold mb-2">
+                        {props.title}
+                    </h5>
+                    <p className="text-inherit text-sm mb-4">{props.desc}</p>
+                </div>
+            </div>
+        </Link>
+    );
+}
+
+export default TutorialElement;
--- pages/course.tsx
+++ pages/course.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import TutorialElement from "../components/TutorialElement";
 
 const course = () => {
-    return <div>course</div>;
+    return (
+        <div className="flex flex-col lg:justify-evenly min-h-screen">
+            <TutorialElement
+                src="/corse.jpg"
+                link="https://hahow.in/courses/60bdbc454ee6592acad8e950/main"
+                title="線上課程｜『PROCREATE插畫入門 － 從素描到風格』"
+                desc="這是我的第一堂在HAHOW好學校上架的線上課程 ，
+                        內容是分享我在學畫圖過程 中體會到的素描的重要性 ，
+                        以及要如何實際去運用素描提升自己的插畫能力 ， 適
+                        合初學者。"
+            />
+            <TutorialElement
+                src="/youtube.jpg"
+                link="https://www.youtube.com/@kookiefunk"
+                title="Youtube｜免費繪畫教學影片"
+                desc="我的Youtube有一些免費的好康 ， 包括繪畫的技巧、想法分享 ， 並且不定時開開直播聊聊天畫
+畫圖 。"
+            />
+        </div>
+    );
 };
 
 export default course;
