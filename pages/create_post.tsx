53fd1ce7a9dc15a754aa31615178bc0508f9b8d8
Django <maja0270558@gmail.com>
2023-02-02 11:44:49 +0800

    Add filepond

--- pages/create_post.tsx
+++ pages/create_post.tsx
@@ -1,7 +1,43 @@
 import React from "react";
 import Error from "next/error";
 
+import { useState } from "react";
+
+// Import React FilePond
+import { FilePond, registerPlugin } from "react-filepond";
+
+// Import FilePond styles
+import "filepond/dist/filepond.min.css";
+
+// Import the Image EXIF Orientation and Image Preview plugins
+// Note: These need to be installed separately
+// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+
+// Register the plugins
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+
 const create_post = () => {
+    const [files, setFiles] = useState([]);
+
+    return (
+        <FilePond
+            imagePreviewMinHeight={80}
+            credits={false}
+            allowProcess={false}
+            imagePreviewMarkupShow={true}
+            instantUpload={false}
+            files={files}
+            onupdatefiles={(fileItems) => {}}
+            allowMultiple={false}
+            maxFiles={1}
+            server="/api"
+            name="files" /* sets the file input name, it's filepond by default */
+            labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+        />
+    );
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content text-center">
