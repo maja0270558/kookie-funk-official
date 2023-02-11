d1f608397c8cde8df316deaef185f984abf7c084
django <maja0270558@gmail.com>
2023-02-11 15:31:38 +0800

    Make DetailLayout scale more reasonable

--- components/DetailLayout.tsx
+++ components/DetailLayout.tsx
@@ -27,7 +27,7 @@ export const DetailLayout = ({
                 height="500"
                 onLoadingComplete={() => setImgIsLoading(false)}
                 sizes="100vw"
-                className="object-contain w-auto h-auto aspect-auto min-h-[80vh] max-h-[85vh]  drop-shadow-md"
+                className="object-contain lg:object-left-bottom min-w-[368px] w-[70vh] h-auto aspect-auto max-h-[85vh]  drop-shadow-md"
             />
         </Skeleton>
     );
@@ -54,20 +54,23 @@ export const DetailLayout = ({
 
 
     return (
-        <div className="flex flex-col m-4 lg:p-20">
-            <div className="flex flex-row place-content-center">
-                <div className="flex flex-col lg:flex-row flex-1 lg:gap-20">
+        <div className=" relative flex flex-col m-4 lg:p-20 min-h-full justify-evenly">
+            <div className=" flex flex-col grow place-content-center">
+                <div className="flex flex-row">
+                    <div className="flex flex-col lg:flex-row flex-1 lg:gap-20">
 
-                    <div className="block align-middle m-auto">{compomentImage}</div>
+                        <div className="block align-middle m-auto">{compomentImage}</div>
 
-                    <div className="flex flex-1 self-stretch min-w-[30%]">
-                        <div className="flex flex-1 lg:items-end">{compomentTitle}</div>
+                        <div className="flex flex-1 self-stretch min-w-[30%]">
+                            <div className="flex flex-1 lg:items-end">{compomentTitle}</div>
+                        </div>
                     </div>
                 </div>
+                <div className=" lg:items-end lg:mt-8">{compomentDesc}</div>
             </div>
-
-            <div className="flex flex-1 lg:items-end lg:mt-8">{compomentDesc}</div>
-            {otherSection}
+            <div className="mb-4">
+                {otherSection}
+            </div>
         </div>
     );
 };
