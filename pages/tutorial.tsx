89fb3757732e675db7991ff7889750bd0e3a4a08
Django <maja0270558@gmail.com>
2023-03-28 15:36:44 +0800

    Publish release 1.1.7
    commit 7dfbaa352fd82f4d98614387a80f4c60bafda724
    Author: Django <maja0270558@gmail.com>
    Date:   Tue Mar 28 15:34:42 2023 +0800
    
        Change tutorial image
    
    commit 6bd21b9916732cb245bc78f31a01b5f3f07c2b8c
    Author: Django <maja0270558@gmail.com>
    Date:   Mon Mar 27 16:09:45 2023 +0800
    
        Modify tutorial page style

--- pages/tutorial.tsx
+++ pages/tutorial.tsx
@@ -1,25 +1,27 @@
+import { Center } from "@mantine/core";
 import React from "react";
 import TutorialElement from "../components/TutorialElement";
 
 const course = () => {
     return (
-        <div className="flex flex-col lg:justify-evenly min-h-screen text-gray-500">
-            <TutorialElement
-                src="/corse.jpg"
-                link="https://hahow.in/cr/kookiefunk"
-                title="線上課程｜『PROCREATE插畫入門 － 從素描到風格』"
-                desc="這是我的第一堂在HAHOW好學校上架的線上課程 ，
-                        內容是分享我在學畫圖過程 中體會到的素描的重要性 ，
-                        以及要如何實際去運用素描提升自己的插畫能力 ， 適
-                        合初學者。"
-            />
-            <TutorialElement
-                src="/youtube.jpg"
-                link="https://www.youtube.com/@kookiefunk"
-                title="Youtube｜免費繪畫教學影片"
-                desc="我的Youtube有一些免費的好康 ， 包括繪畫的技巧、想法分享 ， 並且不定時開開直播聊聊天畫
-畫圖 。"
-            />
+        <div className="container mx-auto p-6 grid grid-rows-1 lg:grid-cols-2 gap-4 drop-shadow-lg lg:mt-20">
+            <div className="col-span-1 flex flex-col p-4 border-2 hover:border-primary bg-base-100">
+                <TutorialElement
+                    src="/course.png"
+                    link="https://hahow.in/cr/kookiefunk"
+                    title="HAHOW 線上課程"
+                    desc="這是我的第一堂在HAHOW好學校上架的線上課程，內容是分享我在學畫圖過程中所體會到素描之重要性，以及要如何實際去運用素描提升自己的插畫能力，適合初學者入門者"
+                />
+            </div>
+            <div className="col-span-1 flex flex-col p-4 border-2 hover:border-primary bg-base-100">
+                <TutorialElement
+                    src="/youtube.png"
+                    link="https://www.youtube.com/@kookiefunk"
+                    title="Youtube"
+                    desc="我的Youtube有一些免費的好康，包括繪畫的技巧、想法分享，並且不定時開開直播聊聊天畫
+畫圖。"
+                />
+            </div>
         </div>
     );
 };
